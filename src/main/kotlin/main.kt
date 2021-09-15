aeabfecd9c7e151a5f9fab4596aa8fe1ecea1c21
MForest7 <goalkeeper14631@gmail.com>
2021-09-16 00:43:59 +0300

    Replaced Pair with data class Modified output format Lines are separated into blocks

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -21,30 +21,36 @@ fun buildEqualsMatrix(old: Array<String>, new: Array<String>): Array<Array<Boole
     return equalsMatrix
 }
 
-fun lcsDP(old: Array<String>, new: Array<String>): Array<Array<String>> {
+enum class From {
+    Old, New, Common, Undefined
+}
+
+data class DiffLine (val file: From, val content: String, val index: Int)
+
+fun lcsDP(old: Array<String>, new: Array<String>): Array<Array<From>> {
     val equals = buildEqualsMatrix(old, new)
 
     val lcs: Array<Array<Int>> = Array(old.size + 1) { Array(new.size + 1) {0} }
-    val lineFrom: Array<Array<String>> = Array(old.size + 1) { Array(new.size + 1) {""} }
+    val lineFrom: Array<Array<From>> = Array(old.size + 1) { Array(new.size + 1) {From.Undefined} }
 
     for (i in old.indices)
-        lineFrom[i][0] = "old"
+        lineFrom[i][0] = From.Old
     for (j in new.indices)
-        lineFrom[0][j] = "new"
+        lineFrom[0][j] = From.New
 
     for (i in old.indices) {
         for (j in new.indices) {
             if (lcs[i + 1][j + 1] <= lcs[i][j + 1]) {
                 lcs[i + 1][j + 1] = lcs[i][j + 1]
-                lineFrom[i + 1][j + 1] = "old"
+                lineFrom[i + 1][j + 1] = From.Old
             }
             if (lcs[i + 1][j + 1] <= lcs[i + 1][j]) {
                 lcs[i + 1][j + 1] = lcs[i + 1][j]
-                lineFrom[i + 1][j + 1] = "new"
+                lineFrom[i + 1][j + 1] = From.New
             }
             if (equals[i][j] and (lcs[i + 1][j + 1] < lcs[i][j] + 1)) {
                 lcs[i + 1][j + 1] = lcs[i][j] + 1
-                lineFrom[i + 1][j + 1] = "common"
+                lineFrom[i + 1][j + 1] = From.Common
             }
         }
     }
@@ -52,44 +58,90 @@ fun lcsDP(old: Array<String>, new: Array<String>): Array<Array<String>> {
     return lineFrom
 }
 
-fun buildDiffArray(old: Array<String>, new: Array<String>, lineFrom: Array<Array<String>>): ArrayList<Pair<String, String>> {
+fun buildDiffLinesArray(old: Array<String>, new: Array<String>, lineFrom: Array<Array<From>>): ArrayList<DiffLine> {
     var iOld = old.size
     var iNew = new.size
 
-    val diffArray: ArrayList<Pair<String, String>> = arrayListOf()
+    val diffLinesArray: ArrayList<DiffLine> = arrayListOf()
 
-    while ((iOld > 0) && (iNew > 0)) {
+    while ((iOld > 0) || (iNew > 0)) {
         when (lineFrom[iOld][iNew]) {
-            "old" -> {
+            From.Old -> {
                 iOld--
-                diffArray.add(Pair("<", old[iOld]))
+                diffLinesArray.add(DiffLine(From.Old, old[iOld], iOld))
             }
-            "new" -> {
+            From.New -> {
                 iNew--
-                diffArray.add(Pair(">", new[iNew]))
+                diffLinesArray.add(DiffLine(From.New, new[iNew], iNew))
             }
-            "common" -> {
+            From.Common -> {
                 iOld--
                 iNew--
-                diffArray.add(Pair("=", old[iOld]))
+                diffLinesArray.add(DiffLine(From.Common, old[iOld], iOld))
             }
             else -> assert(false)
         }
     }
 
-    diffArray.reverse()
+    diffLinesArray.reverse()
 
-    return diffArray
+    return diffLinesArray
 }
 
-fun printDiffArray(diffArray: ArrayList<Pair<String, String>>) {
+data class DiffLineBlock (val file: From, val lines: ArrayList<String>, var firstIndex: Int, var lastIndex: Int)
+
+fun compressLines(linesArray: ArrayList<DiffLine>): ArrayList<DiffLineBlock> {
+    val diffBlocks: ArrayList<DiffLineBlock> = ArrayList()
+    var lastLineFrom = From.Undefined
+    var addedFromOld = 0
+    var addedFromNew = 0
+    for (line in linesArray) {
+        if (line.file == lastLineFrom) {
+            diffBlocks.last().lines.add(line.content)
+            diffBlocks.last().lastIndex = line.index
+        } else {
+            diffBlocks.add(DiffLineBlock(line.file, arrayListOf(line.content), line.index, line.index))
+        }
+        lastLineFrom = line.file
+    }
+    return diffBlocks
+}
+
+fun printDiffLines(diffArray: ArrayList<DiffLine>) {
     for ((flag, line) in diffArray)
-        println("$flag $line")
+        println("${when (flag) {
+            From.Old -> "< "
+            From.New -> "> "
+            From.Common -> "= "
+            From.Undefined -> "UNKNOWN "
+        }        } $line")
+}
+
+fun printDiffArray(diffArray: ArrayList<DiffLineBlock>) {
+    for (block in diffArray) {
+        when (block.file) {
+            From.Old -> println("del${block.firstIndex}-${block.lastIndex}")
+            From.New -> println("add${block.firstIndex}-${block.lastIndex}")
+            else -> continue
+        }
+        for (line in block.lines) {
+            println("${when (block.file) {
+                From.Old -> "< "
+                From.New -> "> "
+                From.Common -> "= "
+                From.Undefined -> "UNKNOWN "
+            }        } $line")
+        }
+    }
 }
 
 fun main(args: Array<String>) {
     val oldContent = readStringsFromFile(readLine()!!)
     val newContent = readStringsFromFile(readLine()!!)
 
-    printDiffArray(buildDiffArray(oldContent, newContent, lcsDP(oldContent, newContent)))
+    val optFrom = lcsDP(oldContent, newContent)
+    val diffLinesArray = buildDiffLinesArray(oldContent, newContent, optFrom)
+    printDiffLines(diffLinesArray)
+    val changedBlocks = compressLines(diffLinesArray)
+    printDiffArray(changedBlocks)
 }
